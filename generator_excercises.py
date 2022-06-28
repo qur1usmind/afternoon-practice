2976398c524ec8564216bf3ee6ddae47573522eb
qur1usmind <qur1usmind@gmail.com>
2022-06-27 23:09:08 -0700

    adding my code for generator and Lambda

--- generator_excercises.py
+++ generator_excercises.py
@@ -2,10 +2,28 @@
 
 #Problem 1
 #Create a generator, primes_gen that generates prime numbers starting from 2.
+#from ctypes import pointer
+#from tkinter import N
 
-gen = primes_gen()
+
+def primes_gen():
+    n=1
+    while True:
+        n+=1
+        for num in range(2,n+1):
+            if n == num:
+                yield n
+            if n%num == 0:
+                break    
+
+
+gen=primes_gen()
 for _ in range(10):
     print(next(gen), end=' ')
+
+#gen=primes_gen()
+#for _ in range(10):
+#    print(next(gen), end=' ')
 # Expected output
 # 2 3 5 7 11 13 17 19 23 29
 #----------------------------------------------------------------------------
@@ -14,8 +32,15 @@ for _ in range(10):
 #Create a generator, unique_letters that generates unique letters from
 #the input string. It should generate the letters in the same order as
 #from the input string.
-
+def unique_letters(mystr):    
+    unique_char=""
+    for letter in mystr:
+       if letter not in unique_char: 
+          unique_char+=letter          
+          yield letter
+             
 for letter in unique_letters('hello'):
-    print(letter, end=' ')
+     print(letter, end=' ')
+#print (myvar)
 # Expected output
 # h e l o
